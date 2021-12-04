e48996c0aff9834685af41337d461d41d7706228
Jerry Dimitriou <jd@inteleproc.eu>
2021-12-04 21:42:19 +0200

    Commented Day01 Code.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,22 @@
 fun main() {
 
     fun part1(input: List<Int>): Int {
-        return input.zipWithNext().count { (i1, i2) -> i2 > i1 }
+        return input
+            .zipWithNext() // Create the pairs for comparison
+            .count { (i1, i2) -> i2 > i1 } // compare and count the successful comparisons
     }
 
     fun part2(input: List<Int>):Int {
-        return input.windowed(3).zipWithNext().count{(l1, l2) -> l1.sum() < l2.sum()}
+        return input
+            .windowed(3) // create 3-element windows
+            .zipWithNext() // create the pairs for comparison
+            .count{(l1, l2) -> l1.sum() < l2.sum()} // compare the 3-element sums and return count
     }
 
-    // test if implementation meets criteria from the description, like:
     // Read the file, create as an array of ints
     val input = readInputAsInts("Day01")
     println(part1(input))
     println(part2(input))
-
 }
 
 
